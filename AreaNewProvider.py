ce563356eca8c2d5c786c6a1406d7d78d1547b83
JulienLux <bezw@qq.com>
2018-05-08 18:37:19 +0800

    ...

--- AreaNewProvider.py
+++ AreaNewProvider.py
@@ -8,3 +8,9 @@ from urllib import request
 from bs4 import BeautifulSoup as bs
 import math
 import xlwt
+
+class Area(object):
+    def __init__(self,name,code,parent):
+        self.name=name
+        self.code=code
+        self.parent=parent
